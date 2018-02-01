20dc0b6f2b45fd26740c36cdc82de8222043adf9
Ryan Kallicharran <Ryan.Kallicharran@ibm.com>
2018-02-01 11:30:11 -0600

    remove unneed componennt and applied patch

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+
+
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { MatButtonModule } from '@angular/material';
+import { MatFileUploadModule } from 'angular-material-fileupload';
+
+
+@NgModule({
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatButtonModule,
+    BrowserModule,
+    MatFileUploadModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string) {
+    const platform = isPlatformBrowser(platformId) ?
+      'on the server' : 'in the browser';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
